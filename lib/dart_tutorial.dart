ca843d87ff9873feeb517b9f82aa77fd9e502925
Hery <thitphavanh.psv@gmail.com>
2022-06-16 05:59:53 +0700

    first commit

---
+++ lib/dart_tutorial.dart
@@ -0,0 +1,30 @@
+void main() {
+  
+  // var boject dynamic
+  // var a = 'abc';
+  // a = 'hello';
+  // a = 123;
+
+  // Object a = 'abc';
+  // a = 'hello';
+  // a = 123;
+
+  // dynamic a = 'abc';
+  // a = 'hello';
+  // a = 123;
+  // a.call();
+
+  // int string bool double datetime ...
+  // String a = 'abc';
+  // a = 123;
+
+  // print(a);
+
+  // null
+  String a;
+  bool b;
+  var c;
+  dynamic d;
+
+  print(123);
+}
