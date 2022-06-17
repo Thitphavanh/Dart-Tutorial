8ff8d2f5a8b4df74a39ff1b9311f31a4a040712d
Hery <thitphavanh.psv@gmail.com>
2022-06-17 13:25:30 +0700

    update

---
+++ lib/ep3_num.dart
@@ -0,0 +1,26 @@
+void main() {
+  // 数值类型
+  // int a = 1;
+  // double b = 1.1;
+  // num c = 1;
+
+  // 表示法
+  // int a = 100; // 十进制
+  // int b = 0xFF; // 十六进制
+  // num c = 2e3; // 科学表示法
+
+  // 数值转换
+  // int a = int.parse('123456');
+  // String b = 123456.toString();
+  // double c = double.parse('12245.2');
+
+  // 与运算
+  // var a = 10;
+  // var b = 2;
+  // print(a & b);
+
+  // 或运算
+  var a = 10;
+  var b = 2;
+  print(a | b);
+}
