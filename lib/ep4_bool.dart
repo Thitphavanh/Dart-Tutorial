b55e33c7900db5b529e46e7630552d3931760143
Hery <thitphavanh.psv@gmail.com>
2022-06-20 04:56:32 +0700

    update

---
+++ lib/ep4_bool.dart
@@ -0,0 +1,25 @@
+void main() {
+  // 使用
+  // bool? a;
+  // if (a ?? false) {
+  //   print('ok');
+  // }
+  // 只有 true 才是真。其它都是 false null
+
+  // 断言
+  // 只有 debuge 调试的时候才生效，发布后就不生效了
+  // bool a = false;
+  // assert(a);
+
+  // String userName = 'Hery';
+  // assert(userName.isNotEmpty);
+
+  // 逻辑操作
+  bool a = true;
+  bool b = false;
+  // assert(a || b);
+  // print(!a);
+
+  // 关系操作
+  // > >= <= > < == !=
+}
