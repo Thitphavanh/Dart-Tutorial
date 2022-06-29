bceae3b8e688ebbca581c56b0ebfe3c2b7d30528
Hery <thitphavanh.psv@gmail.com>
2022-06-30 06:01:24 +0700

    update

---
+++ lib/ep7_list.dart
@@ -0,0 +1,96 @@
+void main() {
+  // 定义
+  // var ls = [1, 2, 3];
+  // print(ls);
+
+  // 自动
+  // var ws = [];
+  // ws
+  //   ..add(1)
+  //   ..add(2)
+  //   ..add(3);
+  // print(ws);
+
+  // 定长
+  // var ws = List<int>.filled(3, 0);
+  // ws[0] = 1;
+  // ws[1] = 2;
+  // ws[2] = 3;
+  // print(ws);
+
+  // 生成数据
+  // var ws = List.generate(3, (i) => i + 2);
+  // print(ws);
+
+  // 属性
+  // var l = [1, 2, 3];
+  // print(l.first);
+  // print(l.last);
+  // print(l.length);
+  // print(l.isEmpty);
+  // print(l.isNotEmpty);
+  // print(l.reversed);
+  // print(l);
+
+  // 方法
+  // 新增
+  // var ls = [];
+  // ls
+  //   ..add(1)
+  //   ..addAll([3, 4])
+  //   ..insert(0, 6)
+  //   ..insertAll(0, [6, 7, 8]);
+  // print(ls);
+
+  // 查询
+  // var ls = [1, 2, 3, 4, 5];
+  // print(ls.indexOf(3));
+  // print(ls.indexWhere((i) {
+  //   return i > 3;
+  // }));
+
+  // 删除
+  // var ls = [1, 2, 3, 4, 5];
+  // ls.remove(3);
+  // print(ls);
+  // ls.removeAt(0);
+  // print(ls);
+
+  // 区间
+  // var ls = [1, 2, 3, 4, 5];
+  // ls.fillRange(0, 3, 99);
+  // print(ls);
+  // print(ls.getRange(0, 3));
+
+  // 洗牌
+  // var ls = [1, 2, 3, 4, 5];
+  // ls.shuffle();
+  // print(ls);
+  // ls.shuffle();
+  // print(ls);
+
+  // 排序
+  // 数字
+  // var ls = [8, 2, 7, 9, 6, 5, 4, 3, 4, 5];
+  // ls.sort();
+  // print(ls);
+
+  // 日期
+  // List<DateTime> dtlist = [];
+  // dtlist.addAll([
+  //   DateTime.now(),
+  //   DateTime.now().add(Duration(days: 1)),
+  //   DateTime.now().add(Duration(days: -2)),
+  // ]);
+  // print(dtlist);
+  // dtlist.sort((a, b) => a.compareTo(b));
+  // print(dtlist);
+
+  // 操作符
+  // var ls = [1, 2, 3, 4, 5];
+  // ls[0] = 99;
+  // print(ls[0]);
+
+  // ls + [6, 7, 8];
+  // print(ls);
+}
